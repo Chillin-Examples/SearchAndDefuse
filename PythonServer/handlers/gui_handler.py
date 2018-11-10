a7c1df4fb986ae0ec85d30d6eda0e23efb5c7875
Amirreza Amouie <aamouie01@gmail.com>
2018-11-10 19:28:46 +0330

    add gui handler base

---
+++ PythonServer/handlers/gui_handler.py
@@ -0,0 +1,9 @@
+class GuiHandler:
+    world = None
+    sides = []
+    canvas = None
+    def __init__(self, world, sides, canvas):
+        pass
+
+    def update(self, gui_events):
+        pass
