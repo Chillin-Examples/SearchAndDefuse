3a9626517831eabd87b30902697de6a90f4add41
Aria <arian_fr2@yahoo.com>
2018-11-13 16:48:20 +0330

    create world board

--- PythonServer/game_handler.py
+++ PythonServer/game_handler.py
@@ -2,55 +2,60 @@
 
 # python imports
 from __future__ import division
-import random
-import json
-import math
-from enum import Enum
 
 # chillin imports
 from chillin_server import TurnbasedGameHandler
-from chillin_server.gui.canvas_elements import ScaleType
-
 # project imports
-from ks.models import (World, Police, Terrorist, Bomb, Position, Constants,
-                       ESoundIntensity, EDirection, ECell)
-from ks.commands import DefuseBomb, PlantBomb, Move, ECommandDirection
 from extensions import *
 
-from .handlers import map_handler, logic_handler, gui_handler
-from .gui_event import gui_event
+from .handlers import map_handler, logic_handler
+from .ks.models import ECell, Police, Terrorist, Constants
 
 
 class GameHandler(TurnbasedGameHandler):
     _map_handler, _logic_handler, _gui_handler = None, None, None
     gui_event_tmp = None
 
-
     def on_recv_command(self, side_name, agent_name, command_type, command):
         if None in command.__dict__.values():
             print("None in command: %s - %s" % (side_name, command_type))
             return
 
-
     def on_initialize(self):
         print('initialize')
-        self.world = World()
+        self._map_handler = map_handler.MapHandler(self.sides)
+        self.world, board = self._map_handler.load_map(Constants)
+        self._logic_handler = logic_handler.LogicHandler(self.world, self.sides)
+        self.world.board = [[ECell.Empty for _ in range(self.world.width)] for _ in range(self.world.height)]
+
+        # Create World board
+        for y in range(self.world.height):
+            for x in range(self.world.width):
+                if board[y][x] == 'w':  # Wall
+                    self.world.board[y][x] = ECell.Wall
+                elif board[y][x] == 'b':  # Small Bomb
+                    self.world.board[y][x] = ECell.SmallBombSite
+                elif board[y][x] == 'l':  # Large Bomb
+                    self.world.board[y][x] = ECell.LargeBombSite
+                elif board[y][x] == 'm':  # Medium Bomb
+                    self.world.board[y][x] = ECell.MediumBombSite
+                elif board[y][x] == 'v':  # Vast Bomb
+                    self.world.board[y][x] = ECell.VastBombSite
+                elif board[y][x] == 'e':  # Empty
+                    self.world.board[y][x] = ECell.Empty
 
 
     def on_initialize_gui(self):
         print('initialize gui')
 
-
     def on_process_cycle(self):
-        print('cycle %i' % (self.current_cycle, ))
+        print('cycle %i' % (self.current_cycle,))
         if self.world.validate_command(None, None):
             self.world.apply_command(None, None)
-
 
     def on_update_clients(self):
         print('update clients')
         self.send_snapshot(self.world)
-
 
     def on_update_gui(self):
         print('update gui')
