68ccb133ad5a2ca233e8ebdc9eb5d3b8e4cf641f
Amirreza Amouie <aamouie01@gmail.com>
2018-11-13 16:40:41 +0330

    initialize models constants

--- PythonServer/handlers/map_handler.py
+++ PythonServer/handlers/map_handler.py
@@ -12,12 +12,11 @@ class MapHandler:
         self.sides = sides
 
     def load_map(self, constants):
-
+        self.initialize_constants(constants)
         self.map_config = json.loads(open((self.config['map']), "r").read())
         board = self.map_config['board']
         self.BOARD_WIDTH = len(board[0])
         self.BOARD_HEIGHT = len(board)
-
         world = World()
         world.width = self.BOARD_WIDTH
         world.height = self.BOARD_HEIGHT
@@ -31,3 +30,6 @@ class MapHandler:
 
 
         return world, board
+
+    def initialize_constants(self, constants):
+        constants.initialize(7, 10, 15, 2, 1, 2, 2.5, 5, 7.5, 10, 5, 4, 6, 100)
