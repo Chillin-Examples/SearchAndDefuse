c67b3cd37f64c24f36de3b1d124390fd92d8017b
Aria <arian_fr2@yahoo.com>
2018-11-14 10:35:26 +0330

    add FogDetector Class

---
+++ PythonServer/utilities/fog.py
@@ -0,0 +1,9 @@
+
+class FogDetector:
+    terrorist_vision_distance=0
+    police_vision_distance=0
+
+    def __init__(self,terrorist_vision_distance,police_vision_distance ):
+        self.police_vision_distance=police_vision_distance
+        self.terrorist_vision_distance=terrorist_vision_distance
+
