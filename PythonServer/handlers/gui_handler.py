34761ff36b3da0e108f92c77c8d97364fa4e6d68
Amirreza Amouie <aamouie01@gmail.com>
2018-11-14 10:59:10 +0330

    draw board on gui_handler's initialize function

--- PythonServer/handlers/gui_handler.py
+++ PythonServer/handlers/gui_handler.py
@@ -1,3 +1,8 @@
+from chillin_server.gui.canvas_elements import ScaleType
+from ..ks.commands import *
+from ..ks.models import *
+
+
 class GuiHandler:
     world = None
     sides = []
@@ -10,5 +15,31 @@ class GuiHandler:
     def update(self, gui_events):
         pass
 
-    def initialize(self):
-        pass
+    def initialize(self, background_ref, canvas, config, world, cell_size,):
+        # Draw background
+        background_ref = self.canvas.create_image('Background', 0, 0)
+        canvas.edit_image(background_ref, scale_type=ScaleType.ScaleToWidth,
+                               scale_value=self.canvas.width - config['statuses_width'])
+
+        # Draw Board
+        for y in range(world.height):
+            for x in range(world.width):
+                cell = world.board[y][x]
+                if cell == ECell.Empty:
+                    self.canvas.create_image('Empty', x * cell_size, y * cell_size,
+                                             scale_type=ScaleType.ScaleToWidth, scale_value=cell_size)
+                elif cell == ECell.Wall:
+                    self.canvas.create_image('Wall', x * cell_size, y * cell_size,
+                                             scale_type=ScaleType.ScaleToWidth, scale_value=cell_size)
+                elif cell == ECell.SmallBombSite:
+                    self.canvas.create_image('SmallBomb', x * cell_size, y * cell_size,
+                                             scale_type=ScaleType.ScaleToWidth, scale_value=cell_size)
+                elif cell == ECell.MediumBombSite:
+                    self.canvas.create_image('MediumBomb', x * cell_size, y * cell_size,
+                                             scale_type=ScaleType.ScaleToWidth, scale_value=cell_size)
+                elif cell == ECell.LargeBombSite:
+                    self.canvas.create_image('LargeBomb', x * cell_size, y * cell_size,
+                                             scale_type=ScaleType.ScaleToWidth, scale_value=cell_size)
+                elif cell == ECell.VastBombSite:
+                    self.canvas.create_image('VastBomb', x * cell_size, y * cell_size,
+                                             scale_type=ScaleType.ScaleToWidth, scale_value=cell_size)
