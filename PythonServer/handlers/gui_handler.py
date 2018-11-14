d72b39b751b4d7f1ad12cf16e9ff139d77d5c6fc
Amirreza Amouie <aamouie01@gmail.com>
2018-11-14 11:06:42 +0330

    rename utilities package and import pos to gui_handler

--- PythonServer/handlers/gui_handler.py
+++ PythonServer/handlers/gui_handler.py
@@ -1,7 +1,7 @@
 from chillin_server.gui.canvas_elements import ScaleType
 from ..ks.commands import *
 from ..ks.models import *
-
+from ..chillin_utilities import pos
 
 class GuiHandler:
     world = None
@@ -43,3 +43,31 @@ class GuiHandler:
                 elif cell == ECell.VastBombSite:
                     self.canvas.create_image('VastBomb', x * cell_size, y * cell_size,
                                              scale_type=ScaleType.ScaleToWidth, scale_value=cell_size)
+
+                # Draw Players
+                for side in self.sides:
+                    img_name = side
+                    for banana in self.world.bananas[side]:
+                        position = pos.Pos(position=banana.position)
+                        canvas_pos = self._get_canvas_position(pos.x, pos.y, center_origin=True)
+                        banana.angle = self.move_angle[EMoveDir.Left.name]
+                        banana.img_ref = self.canvas.create_image(img_name, canvas_pos['x'], canvas_pos['y'],
+                                                                  center_origin=True, scale_type=ScaleType.ScaleToWidth,
+                                                                  scale_value=self.cell_size)
+
+                        text_color = self.canvas.make_rgba(0, 0, 255,
+                                                           255) if side == 'Chiquita' else self.canvas.make_rgba(255, 0,
+                                                                                                                 0, 255)
+                        banana.id_ref = self.canvas.create_text(str(banana.id),
+                                                                canvas_pos['x'] + self.cell_size // 2 - 10,
+                                                                canvas_pos['y'] - self.cell_size // 2, text_color,
+                                                                self.font_size, center_origin=True)
+
+                        x1, y1, x2, y2 = self._get_line_xys(banana, banana.health, banana.max_health, 0)
+                        banana.health_ref = self.canvas.create_line(x1, y1, x2, y2,
+                                                                    self.canvas.make_rgba(255, 0, 0, 150),
+                                                                    stroke_width=5)
+
+                        x1, y1, x2, y2 = self._get_line_xys(banana, banana.laser_count, banana.max_laser_count, 5)
+                        banana.ammo_ref = self.canvas.create_line(x1, y1, x2, y2, self.canvas.make_rgba(0, 0, 255, 150),
+                                                                  stroke_width=5)
