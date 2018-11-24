ccb0b3a5509acb6fe6c155fcad5ca2e38c842e61
Amirreza Amouie <aamouie01@gmail.com>
2018-11-24 18:09:27 +0330

    Revert "FINAL fix"
    This reverts commit 3c1941204057ee3f72fa2dd637b7b0805166a5da.

--- PythonServer/helpers/fog.py
+++ PythonServer/helpers/fog.py
@@ -2,6 +2,8 @@
 
 
 class FogDetector:
+    terrorist_vision_distance=0
+    police_vision_distance=0
 
     def __init__(self,terrorist_vision_distance,police_vision_distance ):
         self.police_vision_distance=police_vision_distance
