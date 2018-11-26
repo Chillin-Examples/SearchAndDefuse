77335b0f9b5a500b99d4624e53a4a9cbec0ac720
Aria <arian_fr2@yahoo.com>
2018-11-26 23:54:31 +0330

    Add _check_move_condition method to extensions.world.py

--- PythonServer/app/extensions/world.py
+++ PythonServer/app/extensions/world.py
@@ -2,10 +2,17 @@
 
 # project imports
 from ..ks.models import World
+from ..ks.commands import Move
 
 
 def apply_command(self, side_name, command):
-    return True
+    # Read Commands
+    if command.name() == Move.name():
+        if not _check_move_condition(self, side_name, command):
+            return False
+        return True
 
+def _check_move_condition(self, side_name, command):
+    pass
 
 World.apply_command = apply_command
