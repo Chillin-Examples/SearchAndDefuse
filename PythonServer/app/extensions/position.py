6d3a679e666143cc143a7fc4538acfae105eda7a
Aria <arian_fr2@yahoo.com>
2018-11-27 23:55:46 +0330

    change isEqual to is_equal

--- PythonServer/app/extensions/position.py
+++ PythonServer/app/extensions/position.py
@@ -4,10 +4,10 @@
 from ..ks.models import Position
 
 
-def isEqual(self, position):
+def is_equal(self, position):
     if self.y == position.y and self.x == position.x:
         return True
     return False
 
 
-Position.isEqual = isEqual
+Position.is_equal = is_equal
