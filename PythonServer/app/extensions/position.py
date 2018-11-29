53fb62efc9b403d3786ecc127c97fe8be1234683
Amirreza Amouie <aamouie01@gmail.com>
2018-11-29 18:17:25 +0330

    overload equal operator for position class

--- PythonServer/app/extensions/position.py
+++ PythonServer/app/extensions/position.py
@@ -4,10 +4,17 @@
 from ..ks.models import Position
 
 
-def is_equal(self, position):
-    if self.y == position.y and self.x == position.x:
+def is_equal(self, another_position):
+    if self.y == another_position.y and self.x == another_position.x:
+        return True
+    return False
+
+
+def __eq__(self, another_position):
+    if self.y == another_position.y and self.x == another_position.x:
         return True
     return False
 
 
 Position.is_equal = is_equal
+Position.__eq__ = __eq__
