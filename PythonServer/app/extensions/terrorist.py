5d7d60b5577f74e3cff2cd6b620b81901bc874c1
Amirreza Amouie <aamouie01@gmail.com>
2018-12-02 21:48:09 +0330

    change explosion remaining time parameter value for plant_bomb

--- PythonServer/app/extensions/terrorist.py
+++ PythonServer/app/extensions/terrorist.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
 # project imports
-from ..ks.models import Terrorist, Bomb
+from ..ks.models import Terrorist, Bomb, Constants
 from ..ks.commands import Move, PlantBomb
 from .agent import move, directions
 
 
 def plant_bomb(self, world, command):
     bomb_position = self.position.add(directions[command.direction.name])
-    world.bombs.append(Bomb(position=bomb_position, explosion_remaining_time=-1))
+    world.bombs.append(Bomb(position=bomb_position, explosion_remaining_time=world.constants.bomb_planting_time))
     # I'm not sure about this yet. If it doesn't work, we should:
     # 1. pass the bomb list instead of world to the function
     # 2. return the updated list and in the world.py file,
