68d1f367fe8344b78e57888ccf866c5c2db691b5
Amirreza Amouie <aamouie01@gmail.com>
2018-12-02 23:14:32 +0330

    remove unncessecary comment

--- PythonServer/app/extensions/terrorist.py
+++ PythonServer/app/extensions/terrorist.py
@@ -9,10 +9,6 @@ from .agent import move, directions
 def plant_bomb(self, world, command):
     bomb_position = self.position.add(directions[command.direction.name])
     world.bombs.append(Bomb(position=bomb_position, explosion_remaining_time=world.constants.bomb_planting_time))
-    # I'm not sure about this yet. If it doesn't work, we should:
-    # 1. pass the bomb list instead of world to the function
-    # 2. return the updated list and in the world.py file,
-    #    write this: self.bombs = plant_bomb(bombs_list, command)
 
 
 Terrorist.plant_bomb = plant_bomb
