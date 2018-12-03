b85a57b694f8cdc1c265ee91d172eebf63d9e0dd
Aria <arian_fr2@yahoo.com>
2018-12-03 12:58:48 +0330

    move World._increase_score_on_plant to scores.py

--- PythonServer/app/extensions/world.py
+++ PythonServer/app/extensions/world.py
@@ -5,6 +5,7 @@ from ..ks.models import *
 from ..ks.commands import *
 from ..gui_events import GuiEvent, GuiEventType
 from .agent import directions
+from ..helpers.score import increase_score_on_plant
 
 
 def apply_command(self, side_name, command):
@@ -29,6 +30,7 @@ def apply_command(self, side_name, command):
         if not self._can_plant(terrorist, command):
             return []
         terrorist.plant_bomb(self, command)
+        self._increase_score_on_plant(terrorist, command)
 
         event_type = GuiEventType.PlantBomb
         return [GuiEvent(event_type, bomb_position=terrorist.position.add(directions[command.direction.name]))]
@@ -70,20 +72,7 @@ def _can_plant(self, terrorist, command):
     return True
 
 
-def _increase_score_on_plant(self, terrorist, command):
-    new_bomb_position = terrorist.position.add(directions[command.direction.name])
-
-    # Update Terrorists Score
-    if self.board[new_bomb_position.y][new_bomb_position.x] == ECell.SmallBombSite:
-        self.score['Terrorist'] += self.constants.score_coefficient_small_bomb_site
-    elif self.board[new_bomb_position.y][new_bomb_position.x] == ECell.MediumBombSite:
-        self.score['Terrorist'] += self.constants.score_coefficient_medium_bomb_site
-    elif self.board[new_bomb_position.y][new_bomb_position.x] == ECell.LargeBombSite:
-        self.score['Terrorist'] += self.constants.score_coefficient_large_bomb_site
-    elif self.board[new_bomb_position.y][new_bomb_position.x] == ECell.VastBombSite:
-        self.score['Terrorist'] += self.constants.score_coefficient_vast_bomb_site
-
-
 World.apply_command = apply_command
 World._can_move = _can_move
 World._can_plant = _can_plant
+World._increase_score_on_plant = increase_score_on_plant
---
+++ PythonServer/app/helpers/score.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# project imports
+from ..ks.models import ECell
+from ..extensions.agent import directions
+
+
+def increase_score_on_plant(world, terrorist, command):
+    new_bomb_position = terrorist.position.add(directions[command.direction.name])
+
+    # Update Terrorists Score
+    if world.board[new_bomb_position.y][new_bomb_position.x] == ECell.SmallBombSite:
+        world.score['Terrorist'] += world.constants.score_coefficient_small_bomb_site
+    elif world.board[new_bomb_position.y][new_bomb_position.x] == ECell.MediumBombSite:
+        world.score['Terrorist'] += world.constants.score_coefficient_medium_bomb_site
+    elif world.board[new_bomb_position.y][new_bomb_position.x] == ECell.LargeBombSite:
+        world.score['Terrorist'] += world.constants.score_coefficient_large_bomb_site
+    elif world.board[new_bomb_position.y][new_bomb_position.x] == ECell.VastBombSite:
+        world.score['Terrorist'] += world.constants.score_coefficient_vast_bomb_site
