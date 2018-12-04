fe60b8011f529dacf702129977281cabfe1599a6
Aria <arian_fr2@yahoo.com>
2018-12-04 12:02:50 +0330

    fix terrorist.plant_bomb

--- PythonServer/app/extensions/terrorist.py
+++ PythonServer/app/extensions/terrorist.py
@@ -7,7 +7,7 @@ from .agent import move, directions
 
 def plant_bomb(self, world, command):
     bomb_position = self.position.add(directions[command.direction.name])
-    world.bombs.append(Bomb(position=bomb_position, explosion_remaining_time=world.constants.bomb_planting_time))
+    world.bombs.append(Bomb(position=bomb_position, explosion_remaining_time=world.constants.bomb_explosion_time))
 
 
 Terrorist.plant_bomb = plant_bomb
