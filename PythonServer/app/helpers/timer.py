7151bd124d9c11a696b6ef12f50a16677f97a014
Amirreza Amouie <aamouie01@gmail.com>
2018-12-04 15:47:21 +0330

    implement _update_bombs_timer_on_cycle

--- PythonServer/app/helpers/timer.py
+++ PythonServer/app/helpers/timer.py
@@ -23,28 +23,42 @@ class BombTimer(Timer):
 
     def __init__(self, world):
         super().__init__(world)
-        self.bombs_timers = {bomb.position: {} for bomb in world.bombs}
+        self._bombs_timers = {bomb.position: {} for bomb in world.bombs}
 
-        for bombsite in self.bombs_timers:
-            bombsite['remaining_planting_time'] = world.constants.bomb_planting_time # this property is for terrorists model in models.ks
-            bombsite['remaining_defusion_time'] = world.constants.bomb_defusion_time # this property is for polices model in models.ks
-            bombsite['remaining_explosion_time'] = world.constants.bomb_explosion_time # this property is for bomb model in models.ks
+        for bombsite in self._bombs_timers:
+            bombsite[
+                'remaining_planting_time'] = world.constants.bomb_planting_time  # this property is for terrorists model in models.ks
+            bombsite['remaining_defusion_time'] = -1  # this property is for polices model in models.ks
+            bombsite['remaining_explosion_time'] = -1  # this property is for bomb model in models.ks
 
         self.planting_queue = queue.Queue()
         self.defusion_queue = queue.Queue()
 
     def update_bombsites_timings(self, active_bombs):
-        for bombsite in self.bombs_timers:
-            for active_bomb in active_bombs:
-                # updating the timers for bombs that are already planted
-                if bombsite == active_bomb:
-                    pass
+        for active_bomb in active_bombs:
+            # if the bomb is planned in this cycle.
+            if active_bomb not in self._bombs_timers:
+                self._update_bombs_timer_on_plant(active_bomb)
 
-                # setting timers for bombs that are being planted in this cycle
-                else:
-                    pass
+            else:
+                # 1. check bombs that are waiting to be planted
+                # 2. check bombs that are already planted
+                self._update_bombs_timer_on_cycle(active_bomb)
 
+    def _update_bombs_timer_on_plant(self, bombsite_position):
+        self._bombs_timers[bombsite_position]['remaining_planting_time'] = self._world.constants.bomb_planting_time
 
-    def _update_timer_on_plant(self, bombsite):
-        # self.bombs_timers[bombsite][]
+    def _update_bombs_timer_on_cycle(self, bombsite_position):
+        # if the planting timer is not zero yet, keep planting
+        if self._bombs_timers[bombsite_position]['remaining_planting_time'] != 0:
+            self._bombs_timers[bombsite_position]['remaining_planting_time'] -= 1
+        else:
+            # if the planting timer is zero, plant the bomb
+            if self._bombs_timers[bombsite_position]['remaining_explosion_time'] == -1:
+                self._bombs_timers[bombsite_position]['remaining_explosion_time'] = self._world.constants.bomb_explosion_time
 
+            # keep counting until the bomb explodes
+            else:
+                self._bombs_timers[bombsite_position]['remaining_explosion_time'] -= 1
+
+            # if it explodes
