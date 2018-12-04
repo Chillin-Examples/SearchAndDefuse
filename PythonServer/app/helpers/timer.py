6d4cb72305bd406b535752c376bfddf70c970bb1
Aria <arian_fr2@yahoo.com>
2018-12-04 20:45:57 +0330

    fix timer.py

--- PythonServer/app/helpers/timer.py
+++ PythonServer/app/helpers/timer.py
@@ -8,18 +8,6 @@ class Timer(object):
         self._world = world
 
 
-class GameTimer(Timer):
-
-    def __init__(self, world):
-        super().__init__(world)
-        self._max_cycles = self._world.constants.max_cycles
-
-    def reached_max_cycles(self, current_cycle_number):
-        if self._max_cycles <= current_cycle_number:
-            return True
-        return False
-
-
 class BombTimer(Timer):
 
     def __init__(self, world):
@@ -30,11 +18,11 @@ class BombTimer(Timer):
         for map_bombs in bombs_queue:
             # if the bomb is planned in this cycle.
             if map_bombs not in self._bombs_timers:
-                self._update_planting_timer_on_plant(map_bombs)
+                self._update_planted_timer_on_plant(map_bombs)
             else:
                 # 1. check bombs that are waiting to be planted
                 # 2. check bombs that are already planted
-                self._update_planting_timer_on_cycle(map_bombs)
+                self._update_planted_timer_on_cycle(map_bombs)
 
     def _update_planted_timer_on_plant(self, bombsite_position):
         self._bombs_timers[bombsite_position]['remaining_planting_time'] = self._world.constants.bomb_planting_time
