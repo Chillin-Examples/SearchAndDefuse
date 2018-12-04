ad78d76b84f6276a7f42b377b4cb1e2353994470
Amirreza Amouie <aamouie01@gmail.com>
2018-12-04 13:17:37 +0330

    add _update_timer_on_plant method header in timer

--- PythonServer/app/helpers/timer.py
+++ PythonServer/app/helpers/timer.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
+import queue
 
 
 class Timer(object):
 
-    def __init(self, world):
+    def __init__(self, world):
         self._world = world
 
 
 class GameTimer(Timer):
 
-    def __init__(self):
+    def __init__(self, world):
+        super().__init__(world)
         self._max_cycles = self._world.constants.max_cycles
 
     def reached_max_cycles(self, current_cycle_number):
@@ -20,15 +22,29 @@ class GameTimer(Timer):
 class BombTimer(Timer):
 
     def __init__(self, world):
+        super().__init__(world)
         self.bombs_timers = {bomb.position: {} for bomb in world.bombs}
 
         for bombsite in self.bombs_timers:
-            bombsite['remaining_planting_time'] = world.constants.bomb_planting_time
-            bombsite['remaining_defusion_time'] = world.constants.bomb_defusion_time
-            bombsite['remaining_explosion_time'] = world.constants.bomb_explosion_time
+            bombsite['remaining_planting_time'] = world.constants.bomb_planting_time # this property is for terrorists model in models.ks
+            bombsite['remaining_defusion_time'] = world.constants.bomb_defusion_time # this property is for polices model in models.ks
+            bombsite['remaining_explosion_time'] = world.constants.bomb_explosion_time # this property is for bomb model in models.ks
 
-    # def check_bombsites(self, bombs):
-    #     for bombsite in self.bombs_timers:
-    #         pass
+        self.planting_queue = queue.Queue()
+        self.defusion_queue = queue.Queue()
 
+    def update_bombsites_timings(self, active_bombs):
+        for bombsite in self.bombs_timers:
+            for active_bomb in active_bombs:
+                # updating the timers for bombs that are already planted
+                if bombsite == active_bomb:
+                    pass
+
+                # setting timers for bombs that are being planted in this cycle
+                else:
+                    pass
+
+
+    def _update_timer_on_plant(self, bombsite):
+        # self.bombs_timers[bombsite][]
 
