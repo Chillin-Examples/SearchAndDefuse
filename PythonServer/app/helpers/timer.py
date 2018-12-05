ae4fbdea06ea190f1649ac8faa3923398c3d0dab
Aria <arian_fr2@yahoo.com>
2018-12-05 18:36:48 +0330

    update timer world

--- PythonServer/app/helpers/timer.py
+++ PythonServer/app/helpers/timer.py
@@ -13,31 +13,33 @@ class BombTimer(Timer):
     def __init__(self, world):
         super().__init__(world)
 
-    def update_bombsites_timings(self, world, agent_id):
-        for bombsite in world.bombs:
+    def update_bombsites_timings(self, world, agent):
+        self._world = world
+        for bombsite in self._world.bombs:
             # if the bomb is planted in this cycle.
-            if world.terrorists[agent_id].remaining_planting_time == -1:
-                self._update_plant_timer_on_plant(agent_id, world)
+            if self._world.terrorists[agent.id].remaining_planting_time == -1:
+                self._update_plant_timer_on_plant(agent.id)
             else:
                 # 1. check bombs that are waiting to be planted
                 # 2. check bombs that are already planted
-                self._update_plant_timer_on_cycle(bombsite, world, agent_id)
+                self._update_plant_timer_on_cycle(bombsite, agent.id)
 
-    def _update_plant_timer_on_plant(self, agent_id, world):
-        world.terrorists[agent_id].planting_remaining_time = world.constants.bomb_planting_time
+    def _update_plant_timer_on_plant(self, agent_id):
+        self._world.terrorists[agent_id].planting_remaining_time = self._world.constants.bomb_planting_time
 
-    def _update_plant_timer_on_cycle(self, bomb, world, terrorist_id):
+    def _update_plant_timer_on_cycle(self, bomb, terrorist_id):
         # if the planting timer is not zero yet, keep planting
-        if world.terrorist[terrorist_id].remaining_planting_time > 0:
-            world.terrorist[terrorist_id].remaining_planting_time -= 1
+        if self._world.terrorist[terrorist_id].remaining_planting_time > 0:
+            self._world.terrorist[terrorist_id].remaining_planting_time -= 1
             return "planting_the_bomb", None
 
         # if it explodes
         elif bomb.explosion_remaining_time <= 0:
             bomb_position = bomb.position
-            del world.bombs[bomb]
+            del self._world.bombs[bomb]
             return "bomb_exploded", bomb_position
 
         # bomb is not exploded yet
         else:
             bomb.explosion_remaining_time -= 1
+
