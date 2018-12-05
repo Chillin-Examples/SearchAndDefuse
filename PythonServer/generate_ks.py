42543706dfb25df2bdfb6b4d00db80b53739ea56
Aria <arian_fr2@yahoo.com>
2018-12-05 18:48:20 +0330

    change generate_ks.py

--- PythonServer/generate_ks.py
+++ PythonServer/generate_ks.py
@@ -2,11 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from koala_serializer import generate
-import sys
+# import sys
 
-lang = 'python'
-if len(sys.argv) > 1:
-    lang = sys.argv[1]
+# if len(sys.argv) > 1:
+#     lang = sys.argv[1]
 
-generate('app/ks/commands.ks', lang, 'app/ks')
-generate('app/ks/models.ks', lang, 'app/ks')
+python_paths = ['app/ks', '../PythonClient/ks', '../PythonRandomClient/ks']
+cpp_path = '../CppClient/Game/ks'
+
+# serialize python
+for path in python_paths:
+    generate('app/ks/commands.ks', 'python', path)
+    generate('app/ks/models.ks', 'python', path)
+
+# serialize cpp
+generate('app/ks/commands.ks', 'cpp', cpp_path)
+generate('app/ks/models.ks', 'cpp', cpp_path)
