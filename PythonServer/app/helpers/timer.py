13b5f0f0276ef5c5e64c7cd33bc0e00030a978f3
Aria <arian_fr2@yahoo.com>
2018-12-05 19:39:50 +0330

    fix update_bombsites_timings algorithm

--- PythonServer/app/helpers/timer.py
+++ PythonServer/app/helpers/timer.py
@@ -15,37 +15,31 @@ class BombTimer(Timer):
 
     def update_bombsites_timings(self, world):
         self._world = world
-        terrorists = self._world.terrorists
-
-        # for defuse
-        polices = self._world.polices
 
         for bombsite in self._world.bombs:
-            for terrorist in terrorists:
                 # if the bomb is planted in this cycle.
-                if self._world.terrorists[terrorist.id].remaining_planting_time == -1:
-                    self._update_plant_timer_on_plant(terrorist.id)
+                if self._world.terrorists[bombsite.planter_id].remaining_planting_time == -1:
+                    self._update_plant_timer_on_plant(bombsite.planter_id)
                 else:
                     # 1. check bombs that are waiting to be planted
                     # 2. check bombs that are already planted
-                    self._update_plant_timer_on_cycle(bombsite, terrorist.id)
+                    # if the planting timer is not zero yet, keep planting
+                    if self._world.terrorist[bombsite.planter_id].remaining_planting_time > 0:
+                        self._world.terrorist[bombsite.planter_id].remaining_planting_time -= 1
+                    else:
+                        self._update_plant_timer_on_cycle(bombsite)
 
     def _update_plant_timer_on_plant(self, agent_id):
         self._world.terrorists[agent_id].planting_remaining_time = self._world.constants.bomb_planting_time
 
-    def _update_plant_timer_on_cycle(self, bomb, terrorist_id):
-        # if the planting timer is not zero yet, keep planting
-        if self._world.terrorist[terrorist_id].remaining_planting_time > 0:
-            self._world.terrorist[terrorist_id].remaining_planting_time -= 1
-            return "planting_the_bomb", None
+    def _update_plant_timer_on_cycle(self, bomb):
 
         # if it explodes
-        elif bomb.planter_id == terrorist_id:
-            if bomb.explosion_remaining_time <= 0:
-                bomb_position = bomb.position
-                del self._world.bombs[bomb]
-                return "bomb_exploded", bomb_position
+        if bomb.explosion_remaining_time <= 0:
+            bomb_position = bomb.position
+            del self._world.bombs[bomb]
+            return "bomb_exploded", bomb_position
 
-            # bomb is not exploded yet
-            else:
-                bomb.explosion_remaining_time -= 1
+        # bomb is not exploded yet
+        else:
+            bomb.explosion_remaining_time -= 1
