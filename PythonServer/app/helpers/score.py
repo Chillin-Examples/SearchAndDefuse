7c13e16e04edc558cb20e2e8478e51d3148cb367
Amirreza Amouie <aamouie01@gmail.com>
2018-12-05 19:55:23 +0330

    modify score.py

--- PythonServer/app/helpers/score.py
+++ PythonServer/app/helpers/score.py
@@ -4,24 +4,24 @@
 from ..ks.models import ECell
 from ..extensions.agent import directions
 
-planted_bombs = []
+handled_bombsites_planted_scores = []
 
 
 def increase_score(operation_type, world, bomb_position):
     if operation_type == "plant":
-        if bomb_position not in planted_bombs:
+        if bomb_position not in handled_bombsites_planted_scores:
             _increase(world, bomb_position, world.constants.bomb_planting_score)
-            planted_bombs.append(bomb_position)
+            handled_bombsites_planted_scores.append(bomb_position)
     elif operation_type == "explode":
         _increase(world, bomb_position, world.constants.bomb_explosion_score)
 
 
 def _increase(world, bomb_position, score):
     if world.board[bomb_position.y][bomb_position.x] == ECell.SmallBombSite:
-        world.scores['Terrorist'] += world.constants.score_coefficient_small_bomb_site * world.bomb_explosion_score
+        world.scores['Terrorist'] += world.constants.score_coefficient_small_bomb_site * score
     elif world.board[bomb_position.y][bomb_position.x] == ECell.MediumBombSite:
-        world.scores['Terrorist'] += world.constants.score_coefficient_medium_bomb_site * world.bomb_explosion_score
+        world.scores['Terrorist'] += world.constants.score_coefficient_medium_bomb_site * score
     elif world.board[bomb_position.y][bomb_position.x] == ECell.LargeBombSite:
-        world.scores['Terrorist'] += world.constants.score_coefficient_large_bomb_site * world.bomb_explosion_score
+        world.scores['Terrorist'] += world.constants.score_coefficient_large_bomb_site * score
     elif world.board[bomb_position.y][bomb_position.x] == ECell.VastBombSite:
-        world.scores['Terrorist'] += world.constants.score_coefficient_vast_bomb_site * world.bomb_explosion_score
+        world.scores['Terrorist'] += world.constants.score_coefficient_vast_bomb_site * score
