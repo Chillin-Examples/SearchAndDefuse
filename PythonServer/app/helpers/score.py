7f3123a8a7b7fd070a5670bbd30e3675347d8486
Amirreza Amouie <aamouie01@gmail.com>
2018-12-05 19:51:49 +0330

    increase score on explode

--- PythonServer/app/helpers/score.py
+++ PythonServer/app/helpers/score.py
@@ -6,15 +6,22 @@ from ..extensions.agent import directions
 
 planted_bombs = []
 
-def increase_score_on_plant(world, bomb_position):
 
-    if bomb_position not in planted_bombs:
-        if world.board[bomb_position.y][bomb_position.x] == ECell.SmallBombSite:
-            world.scores['Terrorist'] += world.constants.score_coefficient_small_bomb_site*world.bomb_planting_score
-        elif world.board[bomb_position.y][bomb_position.x] == ECell.MediumBombSite:
-            world.scores['Terrorist'] += world.constants.score_coefficient_medium_bomb_site*world.bomb_planting_score
-        elif world.board[bomb_position.y][bomb_position.x] == ECell.LargeBombSite:
-            world.scores['Terrorist'] += world.constants.score_coefficient_large_bomb_site*world.bomb_planting_score
-        elif world.board[bomb_position.y][bomb_position.x] == ECell.VastBombSite:
-            world.scores['Terrorist'] += world.constants.score_coefficient_vast_bomb_site*world.bomb_planting_score
-        planted_bombs.append(bomb_position)
+def increase_score(operation_type, world, bomb_position):
+    if operation_type == "plant":
+        if bomb_position not in planted_bombs:
+            _increase_score(world, bomb_position, world.constants.bomb_planting_score)
+            planted_bombs.append(bomb_position)
+    elif operation_type == "explode":
+        _increase_score(world, bomb_position, world.constants.bomb_explosion_score)
+
+
+def _increase_score(world, bomb_position, score):
+    if world.board[bomb_position.y][bomb_position.x] == ECell.SmallBombSite:
+        world.scores['Terrorist'] += world.constants.score_coefficient_small_bomb_site * world.bomb_explosion_score
+    elif world.board[bomb_position.y][bomb_position.x] == ECell.MediumBombSite:
+        world.scores['Terrorist'] += world.constants.score_coefficient_medium_bomb_site * world.bomb_explosion_score
+    elif world.board[bomb_position.y][bomb_position.x] == ECell.LargeBombSite:
+        world.scores['Terrorist'] += world.constants.score_coefficient_large_bomb_site * world.bomb_explosion_score
+    elif world.board[bomb_position.y][bomb_position.x] == ECell.VastBombSite:
+        world.scores['Terrorist'] += world.constants.score_coefficient_vast_bomb_site * world.bomb_explosion_score
