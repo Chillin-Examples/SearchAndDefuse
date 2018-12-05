6d10a9654badb5797371251c7609a514a1d678ff
Aria <arian_fr2@yahoo.com>
2018-12-05 19:06:13 +0330

    remove agent from BombTimer.update_bombsites_timings attrs

--- PythonServer/app/helpers/timer.py
+++ PythonServer/app/helpers/timer.py
@@ -13,16 +13,22 @@ class BombTimer(Timer):
     def __init__(self, world):
         super().__init__(world)
 
-    def update_bombsites_timings(self, world, agent):
+    def update_bombsites_timings(self, world):
         self._world = world
+        terrorists = self._world.terrorists
+
+        # for defuse
+        polices = self._world.polices
+
         for bombsite in self._world.bombs:
-            # if the bomb is planted in this cycle.
-            if self._world.terrorists[agent.id].remaining_planting_time == -1:
-                self._update_plant_timer_on_plant(agent.id)
-            else:
-                # 1. check bombs that are waiting to be planted
-                # 2. check bombs that are already planted
-                self._update_plant_timer_on_cycle(bombsite, agent.id)
+            for terrorist in terrorists:
+                # if the bomb is planted in this cycle.
+                if self._world.terrorists[terrorist.id].remaining_planting_time == -1:
+                    self._update_plant_timer_on_plant(terrorist.id)
+                else:
+                    # 1. check bombs that are waiting to be planted
+                    # 2. check bombs that are already planted
+                    self._update_plant_timer_on_cycle(bombsite, terrorist.id)
 
     def _update_plant_timer_on_plant(self, agent_id):
         self._world.terrorists[agent_id].planting_remaining_time = self._world.constants.bomb_planting_time
