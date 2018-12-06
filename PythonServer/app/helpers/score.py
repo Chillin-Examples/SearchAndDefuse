255d1779a66c78c9f640e93381a020eaa2189655
Aria <arian_fr2@yahoo.com>
2018-12-06 18:59:40 +0330

    rename _increase

--- PythonServer/app/helpers/score.py
+++ PythonServer/app/helpers/score.py
@@ -9,13 +9,13 @@ handled_bombsites_planted_scores = []
 def increase_score(operation_type, world, bomb_position):
     if operation_type == "plant":
         if bomb_position not in handled_bombsites_planted_scores:
-            _increase(world, bomb_position, world.constants.bomb_planting_score)
+            _increase_terrorist_score(world, bomb_position, world.constants.bomb_planting_score)
             handled_bombsites_planted_scores.append(bomb_position)
     elif operation_type == "explode":
-        _increase(world, bomb_position, world.constants.bomb_explosion_score)
+        _increase_terrorist_score(world, bomb_position, world.constants.bomb_explosion_score)
 
 
-def _increase(world, bomb_position, score):
+def _increase_terrorist_score(world, bomb_position, score):
     if world.board[bomb_position.y][bomb_position.x] == ECell.SmallBombSite:
         world.scores['Terrorist'] += world.constants.score_coefficient_small_bomb_site * score
     elif world.board[bomb_position.y][bomb_position.x] == ECell.MediumBombSite:
