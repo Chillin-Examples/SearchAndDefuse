1616e1483a0e4ea20ba185d2e0e550a0418bea77
Aria <arian_fr2@yahoo.com>
2018-12-08 13:07:22 +0330

    implement Police.defuse_bomb

--- PythonServer/app/extensions/police.py
+++ PythonServer/app/extensions/police.py
@@ -1,9 +1,19 @@
 # -*- coding: utf-8 -*-
 
 # project imports
-from ..ks.models import Police, Bomb
+from ..ks.models import Police
 from .agent import move, directions
 
 
 def defuse_bomb(self, world, command):
-    pass
+    defusing_bomb = None
+    bomb_position = self.position.add(directions[command.direction.name])
+    for bomb in world.bombs:
+        if bomb.position == bomb_position:
+            defusing_bomb = bomb
+            break
+    world.bombs[defusing_bomb].defuser_id = self.id
+
+
+Police.defuse_bomb = defuse_bomb
+Police.move = move
