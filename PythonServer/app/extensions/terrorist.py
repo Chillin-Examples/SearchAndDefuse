54fa99b3952e559be1e4b0f507f85f2b6e22e130
Amirreza Amouie <aamouie01@gmail.com>
2018-12-08 18:44:22 +0330

    clean replanting bombs

--- PythonServer/app/extensions/terrorist.py
+++ PythonServer/app/extensions/terrorist.py
@@ -7,18 +7,13 @@ from .agent import move, directions
 
 def plant_bomb(self, world, command):
     bomb_position = self.position.add(directions[command.direction.name])
+
     new_bomb = Bomb(position=bomb_position, explosion_remaining_time=world.constants.bomb_explosion_time,
                     planter_id=self.id, defuser_id=None)
 
     # check replanting condition
     world.bombs[:] = [bomb for bomb in world.bombs if bomb != new_bomb]
     world.bombs.append(new_bomb)
-
-    # for bomb in world.bombs:
-    #     if new_bomb == bomb:
-    #         del world.bombs[bomb]
-    #         break
-    # world.bombs.append(new_bomb)
 
 
 Terrorist.plant_bomb = plant_bomb
