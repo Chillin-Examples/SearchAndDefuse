82ebef0772337a35df36ddc9fa6d82d038064d0a
Aria <arian_fr2@yahoo.com>
2018-12-08 12:53:51 +0330

    remove Police.defuse_bomb body

--- PythonServer/app/extensions/police.py
+++ PythonServer/app/extensions/police.py
@@ -6,9 +6,4 @@ from .agent import move, directions
 
 
 def defuse_bomb(self, world, command):
-    bomb_position = self.position.add(directions[command.direction.name])
-    world.bombs.remove(Bomb(position=bomb_position, explosion_remaining_time=world.constants.bomb_explosion_time))
-
-
-Police.defuse_bomb = defuse_bomb
-Police.move = move
+    pass
