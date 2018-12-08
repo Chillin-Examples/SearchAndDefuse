019b5dfb4bd7b071cb7bd6efd5c5ded99b5e55c5
Aria <arian_fr2@yahoo.com>
2018-12-08 13:33:40 +0330

    implement defuse timer

--- PythonServer/app/helpers/timer.py
+++ PythonServer/app/helpers/timer.py
@@ -22,6 +22,16 @@ class BombTimer(Timer):
                 # defuse command given in current cycle.
                 if self.world.polices[bomb.defuser_id].defusion_remaining_time == -1:
                     self._update_defuse_timer_on_defuse(bomb.defuser_id)
+                else:
+
+                    # defusing timer is not zero yet,police should keep defusing
+                    if self.world.polices[bomb.defuser_id].defusion_remaining_time > 0:
+                        self.world.polices[bomb.defuser_id].defusion_remaining_time -= 1
+
+                    elif self.world.polices[bomb.defuser_id].defusion_remaining_time == 0:
+                        score.increase_score('defuse', world)
+                        del self.world.bombs[bomb]
+                        self.world.polices[bomb.defuser_id].defusion_remaining_time = -1
 
     def _update_defuse_timer_on_defuse(self, agent_id):
         self.world.polices[agent_id].defusion_remaining_time = self.world.constants.bomb_defusion_time
