93d844431bf500f546b4252825664b3ba1595ab4
Aria <arian_fr2@yahoo.com>
2018-12-08 12:44:50 +0330

    add score.py

---
+++ PythonServer/app/helpers/score.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+def increase_score(operation_type, world):
+    if operation_type == "defuse":
+            _increase_police_score(world, world.constants.bomb_defusing_score)
+
+
+def _increase_police_score(world, score):
+        world.scores['Police'] += score
