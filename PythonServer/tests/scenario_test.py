ac0bf5ec2f2dc44ef2dd8e8bfa4afa7d8f9c4a53
Aria <arian_fr2@yahoo.com>
2018-12-11 19:09:08 +0330

    implement plant_bomb_manually

--- PythonServer/tests/scenario_test.py
+++ PythonServer/tests/scenario_test.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# project imports
+from ..app.ks.models import Bomb
 
-def plant_bomb_manualy():
-    pass
+
+def plant_bomb_manually(position, world):
+    new_test_bomb = Bomb(position=position)
+    world.bombs.append(new_test_bomb)
