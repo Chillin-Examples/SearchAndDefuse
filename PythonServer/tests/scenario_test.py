28643ceaf9fcb1c2757a87af536ae88e51023975
Aria <arian_fr2@yahoo.com>
2018-12-11 19:16:41 +0330

    fix plant_bomb_manually

--- PythonServer/tests/scenario_test.py
+++ PythonServer/tests/scenario_test.py
@@ -5,5 +5,8 @@ from ..app.ks.models import Bomb
 
 
 def plant_bomb_manually(position, world):
-    new_test_bomb = Bomb(position=position)
+    new_test_bomb = Bomb(position=position,
+                         explosion_remaining_time=world.constants.bomb_explosion_time,
+                         planter_id=-1, defuser_id=None)
+
     world.bombs.append(new_test_bomb)
