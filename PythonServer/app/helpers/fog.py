a80fbac4b78928c9b08f17125004c56d70edff51
Aria <arian_fr2@gmail.com>
2018-12-13 21:55:14 +0330

    fix fog.py structure

--- PythonServer/app/helpers/fog.py
+++ PythonServer/app/helpers/fog.py
@@ -1,43 +1,38 @@
 # -*- coding: utf-8 -*-
 
-# project imports
-from..ks.models import Position
 
-class FogDetector:
+def _compute_agent_fog(agent):
+    pass
 
-    def __init__(self, terrorist_vision_distance, police_vision_distance):
-        self.police_vision_distance = police_vision_distance
-        self.terrorist_vision_distance = terrorist_vision_distance
 
-    def _compute_agent_fog(self, agent):
-        pass
+def _compute_polices_fogs(positions):
+    pass
 
-    def _compute_polices_fogs(self, positions):
-        pass
 
-    def compute_terrorists_fogs(self, positions):
-        pass
+def compute_terrorists_fogs(positions):
+    pass
 
-    # TODO fix dls algorithm
-    def _depth_limited_search(self, position, limit):
-        sentinel = object()
-        visited_stack = [position]
-        path = []
 
-        while visited_stack:
-            current_position = visited_stack.pop()
+# TODO fix dls algorithm
+def _depth_limited_search(position, limit):
+    sentinel = object()
+    visited_stack = [position]
+    path = []
 
-            if current_position == sentinel:
+    while visited_stack:
+        current_position = visited_stack.pop()
 
-                # finished this level; go back up one level
-                limit += 1
-                path.pop()
+        if current_position == sentinel:
 
-            elif limit != 0:
+            # finished this level; go back up one level
+            limit += 1
+            path.pop()
 
-                # go one level deeper, push sentinel
-                limit -= 1
-                path.append(current_position)
-                visited_stack.append(sentinel)
-                # visited_stack.extend()
-        return path
+        elif limit != 0:
+
+            # go one level deeper, push sentinel
+            limit -= 1
+            path.append(current_position)
+            visited_stack.append(sentinel)
+            visited_stack.extend(current_position.get_neighbours)
+    return path
