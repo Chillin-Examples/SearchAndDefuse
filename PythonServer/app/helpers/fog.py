634f114d52c67ffde9581da9e5ff3f7987e9b6b9
Aria <arian_fr2@gmail.com>
2018-12-13 19:58:21 +0330

    add methods to FogDetector class

--- PythonServer/app/helpers/fog.py
+++ PythonServer/app/helpers/fog.py
@@ -6,3 +6,12 @@ class FogDetector:
     def __init__(self, terrorist_vision_distance, police_vision_distance):
         self.police_vision_distance = police_vision_distance
         self.terrorist_vision_distance = terrorist_vision_distance
+
+    def compute_agent_fog(self, agent):
+        pass
+
+    def compute_polices_fogs(self, positions):
+        pass
+
+    def compute_terrorists_fogs(self, positions):
+        pass
