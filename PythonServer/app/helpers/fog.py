b2e54a97cd47ae85f3be2a60c222919ae4850d57
Aria <arian_fr2@gmail.com>
2018-12-13 20:25:05 +0330

    add _depth_limited_search method

--- PythonServer/app/helpers/fog.py
+++ PythonServer/app/helpers/fog.py
@@ -7,11 +7,14 @@ class FogDetector:
         self.police_vision_distance = police_vision_distance
         self.terrorist_vision_distance = terrorist_vision_distance
 
-    def compute_agent_fog(self, agent):
+    def _compute_agent_fog(self, agent):
         pass
 
-    def compute_polices_fogs(self, positions):
+    def _compute_polices_fogs(self, positions):
         pass
 
     def compute_terrorists_fogs(self, positions):
+        pass
+
+    def _depth_limited_search(self, position):
         pass
