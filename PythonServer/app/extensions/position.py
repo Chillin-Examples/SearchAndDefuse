662a3d10021c3249cb6e4710df86987449e872ea
Aria <arian_fr2@gmail.com>
2018-12-13 21:52:52 +0330

    add get_neighbours method to position.py :|

--- PythonServer/app/extensions/position.py
+++ PythonServer/app/extensions/position.py
@@ -5,7 +5,9 @@ from ..ks.models import Position
 
 
 def is_equal(self, position):
-    return self.x == position.x and self.y == position.y
+    if self.y == position.y and self.x == position.x:
+        return True
+    return False
 
 
 def __eq__(self, position):
@@ -13,17 +15,22 @@ def __eq__(self, position):
 
 
 def add(self, position):
-    return Position(
-        x=self.x + position.x,
-        y=self.y + position.y
-    )
+    x = self.x + position.x
+    y = self.y + position.y
+    return Position(x=x, y=y)
 
 
 def __add__(self, position):
     return self.add(position)
 
 
+def get_neighbours(self, position):
+    return [Position(x=self.x - 1, y=self.y), Position(x=self.x + 1, y=self.y),
+            Position(x=self.x, y=self.y - 1), Position(x=self.x, y=self.y + 1)]
+
+
 Position.is_equal = is_equal
 Position.__eq__ = __eq__
 Position.add = add
 Position.__add__ = __add__
+Position.get_neighbours = get_neighbours
