4e4eb814002b45178d7342c3408bd2048d67a06d
Amirreza Amouie <aamouie01@gmail.com>
2018-12-15 11:43:54 +0330

    resolve conflicts in terrorists.py, score.py, terrorist.py, and world.py

--- PythonServer/app/extensions/terrorist.py
+++ PythonServer/app/extensions/terrorist.py
@@ -1,15 +1,32 @@
 # -*- coding: utf-8 -*-
 
 # project imports
-from ..ks.models import Terrorist
-from ..ks.commands import Move, PlantBomb
-from .agent import move
+from ..ks.models import Terrorist, Bomb
+from .agent import directions, move as base_move
+
+
+def move(self, world, command):
+    if self.planting_remaining_time != -1:
+        self.cancel_plant(self, world)
+    base_move(self, world, command)
 
 
 def plant_bomb(self, world, command):
-    # defuser_id should be -1 at first.
-    return True
+    if self.planting_remaining_time != -1:
+        self.cancel_plant(world)
+
+    bomb_position = self.position + directions[command.direction.name]
+    new_bomb = Bomb(position=bomb_position, explosion_remaining_time=-1,
+                    planter_id=self.id, defuser_id=None)
+    world.bombs.append(new_bomb)
 
 
-Terrorist.defuse_bomb = plant_bomb
+def cancel_plant(self, world):
+    bomb = next((bomb for bomb in world.bombs if bomb.planter_id == self.id))
+    self.bombs.remove(bomb)
+    self.planting_remaining_time = -1
+
+
+Terrorist.plant_bomb = plant_bomb
 Terrorist.move = move
+Terrorist.cancel_plant = cancel_plant
--- PythonServer/app/extensions/world.py
+++ PythonServer/app/extensions/world.py
@@ -25,6 +25,20 @@ def apply_command(self, side_name, command):
         event_type = GuiEventType.MovePolice if side_name == 'Police' else GuiEventType.MoveTerrorist
         return [GuiEvent(event_type, agent_id=agent.id, agent_position=agent.position)]
 
+    elif command.name() == PlantBomb.name():
+        # Only terrorists can plan
+        if side_name == "Police":
+            return []
+
+        terrorist = agents["Terrorist"][command.id]
+        if not self._can_plant_bomb(terrorist, command):
+            return []
+        terrorist.plant_bomb(self, command)
+
+        # TODO event should be matched with bomb when plantation is done
+        event_type = GuiEventType.PlantingBomb
+        return [GuiEvent(event_type, bomb_position=terrorist.position.add(directions[command.direction.name]))]
+
     elif command.name() == DefuseBomb.name():
         # Only terrorists can plan
         if side_name == "Terrorist":
@@ -55,6 +69,43 @@ def _can_move_agent(self, side_name, agent, command):
 
     return False
 
+def _can_move_agent(self, side_name, agent, command):
+    new_position = agent.position.add(directions[command.direction.name])
+
+    # Check new cell is empty
+    if self.board[new_position.y][new_position.x] == ECell.Empty:
+        # Check No Teammate Is There
+        teammates = self.polices if side_name == 'Police' else self.terrorists
+
+        for teammate in teammates:
+            if teammate.position == new_position:
+                return False
+
+        return True
+
+    return False
+
+
+def _can_plant_bomb(self, terrorist, command):
+    new_bomb_position = terrorist.position.add(directions[command.direction.name])
+
+    # If it's not a bombsite return false
+    if self.board[new_bomb_position.y][new_bomb_position.x] not in [ECell.SmallBombSite, ECell.MediumBombSite,
+                                                                    ECell.LargeBombSite, ECell.VastBombSite]:
+        return False
+
+    # If it already has a bomb with different planter
+    for planted_bomb in self.bombs:
+        if planted_bomb.position == new_bomb_position and planted_bomb.planter_id != terrorist.id:
+            return False
+
+        # if bomb is exploding
+        if planted_bomb.explosion_remaining_time != -1:
+            return False
+
+    # Otherwise return True!
+    return True
+
 
 def _can_defuse_bomb(self, police, command):
     planted_position = police.position.add(directions[command.direction.name])
@@ -74,3 +125,4 @@ def _can_defuse_bomb(self, police, command):
 World.apply_command = apply_command
 World._can_move_agent = _can_move_agent
 World._can_defuse_bomb = _can_defuse_bomb
+World._can_plant_bomb = _can_plant_bomb
