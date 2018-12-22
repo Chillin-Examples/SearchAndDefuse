d194abf1c962949672d74b884f3e32565456bff1
Aria <arian_fr2@gmail.com>
2018-12-22 20:26:26 +0330

    update generate_ks file

--- PythonServer/generate_ks.py
+++ PythonServer/generate_ks.py
@@ -4,9 +4,10 @@
 # chillin imports
 from koala_serializer import generate
 
-all_args = [('python', 'app/ks'), ('python', '../PythonClient/ks'), ('python', '../PythonRandomClient/ks'),
-            ('cpp', '../CppClient/Game/ks')]
+all_args = [('python', 'app/ks', 'snake_case'), ('python', '../PythonClient/ks', 'snake_case'),
+            ('python', '../PythonRandomClient/ks', 'snake_case'),
+            ('cpp', '../CppClient/Game/ks', 'camelCase')]
 
 for args in all_args:
-    generate('app/ks/commands.ks', *args, 'snake_case')
-    generate('app/ks/models.ks', *args, 'snake_case')
+    generate('app/ks/commands.ks', *args)
+    generate('app/ks/models.ks', *args)
