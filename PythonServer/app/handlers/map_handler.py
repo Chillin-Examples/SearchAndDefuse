4d92efdf983834b814a3a7dd5c69ad66a4f4a76e
Aria <arian_fr2@gmail.com>
2018-12-23 19:58:04 +0330

    initialize agents status

--- PythonServer/app/handlers/map_handler.py
+++ PythonServer/app/handlers/map_handler.py
@@ -64,7 +64,7 @@ class MapHandler:
                     new_police.defusion_remaining_time = -1
                     new_police.footstep_sounds = []
                     new_police.bomb_sounds = []
-                    new_police.is_visible = False
+                    new_police.status = Status.Alive
                     world.polices.append(new_police)
                 if side == 'Terrorist':
                     new_terrorist = Terrorist()
@@ -72,7 +72,7 @@ class MapHandler:
                     new_terrorist.position = player_position
                     new_terrorist.planting_remaining_time = -1
                     new_terrorist.footstep_sounds = []
-                    new_terrorist.is_dead = False
+                    new_terrorist.status = Status.Alive
                     world.terrorists.append(new_terrorist)
 
     def load_map(self, config):
