e11e9d67fc40a1acdfe3f38ece296cff21ac27be
Aria <arian_fr2@gmail.com>
2018-12-24 17:28:38 +0330

    add position to payload

--- PythonServer/app/extensions/world.py
+++ PythonServer/app/extensions/world.py
@@ -27,7 +27,8 @@ def apply_command(self, side_name, command):
                         if terrorist.position == police_vision:
                             terrorist.status = Status.Dead
                             terrorist_death_events.append(GuiEvent(GuiEventType.TerroristDeath,
-                                                                   terrorist_id=terrorist.id))
+                                                                   terrorist_id=terrorist.id,
+                                                                   position=terrorist.position))
                             self.visions["Terrorist"] = vision.compute_terrorists_visions(self)
 
             # update world visions
