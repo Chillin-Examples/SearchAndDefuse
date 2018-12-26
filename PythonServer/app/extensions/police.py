4e7f73fffc074b2d4d4013a585f22f2061936e07
Amirreza Amouie <aamouie01@gmail.com>
2018-12-26 16:49:01 +0330

    change footstep's file name

--- PythonServer/app/extensions/police.py
+++ PythonServer/app/extensions/police.py
@@ -4,7 +4,7 @@
 from ..ks.models import Police
 from .agent import directions, can_move as base_can_move, move as base_move
 from ..gui_events import GuiEventType, GuiEvent
-from ..helpers import sound_intensity
+from ..helpers import footsteps
 
 
 def move(self, world, command):
@@ -13,7 +13,7 @@ def move(self, world, command):
         gui_events += self.cancel_defuse(world)
 
     base_move(self, world, command)
-    sound_intensity.update_police_intensities(self)
+    footsteps.update_police_intensities(self)
 
     gui_events += [GuiEvent(GuiEventType.MovePolice, agent_id=self.id, agent_position=self.position)]
     return gui_events
