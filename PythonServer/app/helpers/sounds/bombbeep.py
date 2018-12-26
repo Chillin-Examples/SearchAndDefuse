2190b896f9ed237f3d81a4c3bed9f007fbbe8f99
Amirreza Amouie <aamouie01@gmail.com>
2018-12-26 17:35:54 +0330

    empty police's bomb sounds list on each cycle

--- PythonServer/app/helpers/sounds/bombbeep.py
+++ PythonServer/app/helpers/sounds/bombbeep.py
@@ -5,8 +5,8 @@ from . import utils
 
 
 def update_bomb_sounds(world):
-    bombs_beeps = []
     explosion_time = world.constants.bomb_explosion_time
+    _empty_police_bomb_sound_list(world.polices)
 
     for bomb in world.bombs:
         bomb_sound = 0
@@ -20,3 +20,8 @@ def update_bomb_sounds(world):
             distance = utils.calculate_distance(bomb.position, police.position)
             value = int(bomb_sound*0.1 + distance*0.9)
             police.bomb_sounds.append(utils.convert_to_enum(value, world.constants.sound_ranges))
+
+
+def _empty_police_bomb_sound_list(polices):
+    for police in polices:
+        police.bomb_sounds = []
