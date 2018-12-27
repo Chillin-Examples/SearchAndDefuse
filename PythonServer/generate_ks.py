75dc3f4f6b64e1299f2423a4279401a9e646d37f
Aria <arian_fr2@gmail.com>
2018-12-28 02:49:04 +0330

    modify generate_ks

--- PythonServer/generate_ks.py
+++ PythonServer/generate_ks.py
@@ -1,12 +1,20 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# python imports
+from shutil import copy
+
 # chillin imports
 from koala_serializer import generate
 
-all_args = [('python', 'app/ks', 'snake_case'), ('python', '../PythonClient/ks', 'snake_case'),
+all_args = [('python', 'app/ks', 'snake_case'),
+            ('python', '../PythonClient/ks', 'snake_case'),
             ('python', '../PythonRandomClient/ks', 'snake_case'),
             ('cpp', '../CppClient/Game/ks', 'camelCase')]
+
+for i in range(1, 4):
+        copy('app/ks/models.ks', all_args[i][1])
+        copy('app/ks/commands.ks', all_args[i][1])
 
 for args in all_args:
     generate('app/ks/commands.ks', *args)
