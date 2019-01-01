8827811a9d6af5ad8777b65c02413b3fc25b3a43
Amirreza Amouie <aamouie01@gmail.com>
2019-01-01 18:32:12 +0330

    debug footstep

--- PythonServer/app/helpers/sounds/footsteps.py
+++ PythonServer/app/helpers/sounds/footsteps.py
@@ -19,7 +19,7 @@ def _update_agent_sound_intensity(agent, opponent_agents, max_intensities_dict):
     for opponent_agent in opponent_agents:
         distance = utils.calculate_distance(agent.position, opponent_agent.position)
         intensity = utils.calculate_intensity(distance)
-        sounds.append(utils.convert_to_enum(intensity, max_intensities_dict))
+        sounds.append(utils.map_to_enum(intensity, max_intensities_dict))
     return sounds
 
 
