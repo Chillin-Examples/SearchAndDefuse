7ef7587f97c8cc9dc78419b3faecf9719fc309a9
Aria <arian_fr2@gmail.com>
2019-01-15 13:28:34 +0330

    fix bombbeep.py

--- PythonServer/app/helpers/sounds/bombbeep.py
+++ PythonServer/app/helpers/sounds/bombbeep.py
@@ -5,20 +5,12 @@ from . import utils
 
 
 def update_police_bomb_sounds(world):
-    explosion_time = world.constants.bomb_explosion_time
     _empty_polices_bomb_sound_list(world.polices)
 
     for bomb in world.bombs:
-        bomb_sound = 0
-        if bomb.explosion_remaining_time != -1:
-            for sound_range in range(1, len(world.constants.sound_ranges)):
-                if (sound_range*explosion_time/sound_range) <= bomb.explosion_remaining_time < ((sound_range+1)*explosion_time/sound_range):
-                    bomb_sound = (sound_range+1)
-                    break
-
         for police in world.polices:
             distance = utils.calculate_distance(bomb.position, police.position)
-            value = int(bomb_sound*0.0 + distance*1.0)
+            value = int(distance)
             police.bomb_sounds.append(utils.int_to_intensity(value, world.constants.sound_ranges))
 
 
